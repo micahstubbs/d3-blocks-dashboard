a5925bdfd3be6597b084397a05634d004e6b980b
Micah Stubbs <micah.stubbs@gmail.com>
2022-08-25 13:41:08 -0700

    add WIP 2d network chart with data from 3d chart

---
+++ 07-dashboard-with-focus-chart/context.js
@@ -0,0 +1,23 @@
+const elem = document.getElementById("3d-graph");
+
+const { width: parentWidth, height: parentHeight } =
+  elem.parentElement.getBoundingClientRect();
+
+const Graph = ForceGraph3D()(elem)
+  .width(parentWidth)
+  .height(parentHeight)
+  .jsonUrl("blocks-citation-graph.json")
+  .nodeLabel((node) => `${node.description} from ${node.user}`)
+  .nodeAutoColorBy("user")
+  .onNodeHover((node) => (elem.style.cursor = node ? "pointer" : null))
+  .onNodeClick((node) => {
+    // Aim at node from outside it
+    const distance = 40;
+    const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
+
+    Graph.cameraPosition(
+      { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+      node, // lookAt ({ x, y, z })
+      3000 // ms transition duration
+    );
+  });
