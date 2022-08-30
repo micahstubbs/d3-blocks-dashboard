2ca9bc49470f18c6c667de7154649eed0cadb472
Micah Stubbs <micah.stubbs@gmail.com>
2022-08-30 13:06:48 -0700

    zoomCameraToNode in a function

--- iterations/08-dashboard-with-detail-view/context.js
+++ iterations/08-dashboard-with-detail-view/context.js
@@ -7,6 +7,38 @@ const elem = document.getElementById("3d-graph");
 const { width: parentWidth, height: parentHeight } =
   elem.parentElement.getBoundingClientRect();
 
+function zoomCameraToNode(node) {
+  // Aim at node from outside it
+  const distance = 1000;
+  const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
+  Graph.cameraPosition(
+    { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+    node, // lookAt ({ x, y, z })
+    3000 // ms transition duration
+  );
+  console.log("zoomed to node", node);
+  const contextNodeClickEvent = new CustomEvent("context-node-click", {
+    detail: node,
+  });
+  elem.dispatchEvent(contextNodeClickEvent);
+  elem.addEventListener(
+    "context-node-click",
+    (e) => {
+      console.log("event from context node click", e);
+      window.selectedNode = e.detail;
+    },
+    false
+  );
+}
+
+const intialNode = {
+  id: "f849f374f21c490c5490d501636bdb77",
+  user: "curran",
+  createdAt: "2017-09-13T09:01:39Z",
+  updatedAt: "2017-09-14T14:23:04Z",
+  description: "Data Table Summary",
+};
+
 const Graph = ForceGraph3D()(elem)
   .width(parentWidth)
   .height(parentHeight)
@@ -16,26 +48,6 @@ const Graph = ForceGraph3D()(elem)
   .onNodeHover((node) => (elem.style.cursor = node ? "pointer" : null))
   .onNodeClick((node) => {
     if (config.animateCameraOnClick) {
-      // Aim at node from outside it
-      const distance = 1000;
-      const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
-      Graph.cameraPosition(
-        { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
-        node, // lookAt ({ x, y, z })
-        3000 // ms transition duration
-      );
+      zoomCameraToNode(node);
     }
-
-    const contextNodeClickEvent = new CustomEvent("context-node-click", {
-      detail: node,
-    });
-    elem.dispatchEvent(contextNodeClickEvent);
-    elem.addEventListener(
-      "context-node-click",
-      (e) => {
-        console.log("event from context node click", e);
-        window.selectedNode = e.detail;
-      },
-      false
-    );
   });
