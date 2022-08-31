b50fdc99827876957c6c5191a11bb589a745e144
Micah Stubbs <micah.stubbs@gmail.com>
2022-08-30 20:51:24 -0700

    restore onclick interaction between context and focus

--- iterations/08-dashboard-with-detail-view/context.js
+++ iterations/08-dashboard-with-detail-view/context.js
@@ -7,30 +7,6 @@ const elem = document.getElementById("3d-graph");
 const { width: parentWidth, height: parentHeight } =
   elem.parentElement.getBoundingClientRect();
 
-function zoomCameraToNode(node) {
-  // Aim at node from outside it
-  const distance = 1000;
-  const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
-  Graph.cameraPosition(
-    { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
-    node, // lookAt ({ x, y, z })
-    3000 // ms transition duration
-  );
-  console.log("zoomed to node", node);
-  const contextNodeClickEvent = new CustomEvent("context-node-click", {
-    detail: node,
-  });
-  elem.dispatchEvent(contextNodeClickEvent);
-  elem.addEventListener(
-    "context-node-click",
-    (e) => {
-      console.log("event from context node click", e);
-      window.selectedNode = e.detail;
-    },
-    false
-  );
-}
-
 // TODO: zoom the camera to the intial node
 // TODO: defined currently in focus.js
 // TODO: after the initial physics simulation
@@ -44,6 +20,27 @@ const Graph = ForceGraph3D()(elem)
   .onNodeHover((node) => (elem.style.cursor = node ? "pointer" : null))
   .onNodeClick((node) => {
     if (config.animateCameraOnClick) {
-      zoomCameraToNode(node);
+      // Aim at node from outside it
+      const distance = 1000;
+      const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
+      Graph.cameraPosition(
+        { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+        node, // lookAt ({ x, y, z })
+        3000 // ms transition duration
+      );
     }
+
+    console.log("clicked node from context", node);
+    const contextNodeClickEvent = new CustomEvent("context-node-click", {
+      detail: node,
+    });
+    elem.dispatchEvent(contextNodeClickEvent);
+    elem.addEventListener(
+      "context-node-click",
+      (e) => {
+        console.log("event from context node click", e);
+        window.selectedNode = e.detail;
+      },
+      false
+    );
   });
